601a5a462e1d173e19c9555b5357f83ab2e55065
telegeyN <ntelegey@gmail.com>
2025-04-17 01:00:44 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Telegey Nurbekova on 17/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
